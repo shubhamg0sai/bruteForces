ba7455027042729833a6a8d41d0633b870704fab
sHuBhAm_G0sAiN <63947251+ShuBhamg0sain@users.noreply.github.com>
2020-10-19 08:12:45 +0530

    Create ap.sh

---
+++ ap.sh
@@ -0,0 +1,13 @@
+
+R = '\033[1;31m'
+H = '\033[1;32m'
+
+echo    "Facebook"
+	print("\n "+R+"["+H+"B"+R+"] "+H+"HotMail ")
+	print("\n "+R+"["+H+"C"+R+"] "+H+"Yahoo ")
+	print("\n "+R+"["+H+"D"+R+"] "+H+"Gmail ")
+	print("\n "+R+"["+H+"X"+R+"] "+H+"Exit ")
+	print("\n "+R+"["+H+"Note"+R+"] "+H+"Tools Brute Force")
+	
+	
+
