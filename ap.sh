4b00ca342f694b9def523051c18dc1eec23538bb
sHuBhAm_G0sAiN <63947251+ShuBhamg0sain@users.noreply.github.com>
2020-10-19 08:32:26 +0530

    Update ap.sh

--- ap.sh
+++ ap.sh
@@ -1,13 +1,34 @@
+#data/data/com.termux/files/usr/bin/bash
+#colours
+red='\033[1;31m'
+rset='\033[0m'
+grn='\033[1;32m'
+ylo='\033[1;33m'
+blue='\033[1;34m'
+cyan='\033[1;36m'
+pink='\033[1;35m'
+#coding section starts :)
 
-R = '\033[1;31m'
-H = '\033[1;32m'
 
-echo    "Facebook"
-	print("\n "+R+"["+H+"B"+R+"] "+H+"HotMail ")
-	print("\n "+R+"["+H+"C"+R+"] "+H+"Yahoo ")
-	print("\n "+R+"["+H+"D"+R+"] "+H+"Gmail ")
-	print("\n "+R+"["+H+"X"+R+"] "+H+"Exit ")
-	print("\n "+R+"["+H+"Note"+R+"] "+H+"Tools Brute Force")
-	
-	
-
+#SCRIPT BY N17R0 DONT TRY TO COPY OR MODIFY 
+#M-WIZ TOOL FOR ALL METASPLOIT THINGS
+#data/data/com.termux/files/usr/bin/bash
+#colours
+red='\033[1;31m'
+rset='\033[0m'
+grn='\033[1;32m'
+ylo='\033[1;33m'
+blue='\033[1;34m'
+cyan='\033[1;36m'
+pink='\033[1;35m'
+#coding section starts :)
+clear
+echo " "
+echo " "
+echo -e "$red
+                         ██████_____████████________██_________██████_________██___██████
+                         ██     █____██___________██_____██_____██     █_____██______██                
+                         ███████____████████___██________██___██████_____█_________██████
+                         ██     █____██___________██______██____██  █_______██_______██
+                         ██████_____██_____________██__██______██    █_________██___██████ 
+                                                                                               v 1.1$rset"                         
