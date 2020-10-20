48ef6beb9860fead4c0bd569cfeea518858179ad
sHuBhAm_G0sAiN <63947251+ShuBhamg0sain@users.noreply.github.com>
2020-10-20 21:12:53 +0530

    Create SG.sh

---
+++ SG.sh
@@ -0,0 +1,41 @@
+#data/data/com.termux/files/usr/bin/bash
+#colours
+red='\033[1;31m'
+rset='\033[0m'
+grn='\033[1;32m'
+ylo='\033[1;33m'
+blue='\033[1;34m'
+cyan='\033[1;36m'
+pink='\033[1;35m'
+#########
+clear
+echo " "
+echo " "
+echo -e "$grn FIND ME $rset"
+echo -e "{ctrl c} exit"
+echo -e "$red What app:"
+echo -e "https://chat.whatsapp.com/JtCW38B01hjAGwlVHhyu5q"
+
+echo -e "       OR "
+
+echo -e "https://chat.whatsapp.com/JyqQKyXuw3f43Ll90pHSMO"
+
+echo -e "      OR "
+
+echo -e "https://chat.whatsapp.com/L4iSBfleMKqKd1G10f7IIc"
+
+
+
+echo -e "pH  919557777030"
+
+echo -e "Instagram : https://www.instagram.com/shubham_g0sain/?hl=en"
+
+echo -e "Github.   : https://github.com/ShuBhamg0sain"
+
+echo -e "Facebook  : https://m.facebook.com/shubham.gosain.980"
+
+echo -e "Facebook group : https://facebook.com/groups/226519011789922/?ref=group_browse" 
+
+echo -e "Telegram :"
+echo -e "https://t.me/ShuBham_g0sain" 
+                                                                                      $rset"
