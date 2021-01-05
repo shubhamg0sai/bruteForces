98e394ad5c6a43b7b99874642f7b7ccb4f6e7376
BLACK-KILLER <63947251+ShuBhamg0sain@users.noreply.github.com>
2021-01-05 14:14:25 +0530

    Update ap.sh

--- ap.sh
+++ ap.sh
@@ -1,260 +1,73 @@
 #SCRIPT BY ShuBhamg0sain DONT TRY TO COPY OR MODIFY 
-#
-#data/data/com.termux/files/usr/bin/bash
-#colours
-red='\033[1;31m'
-rset='\033[0m'
-grn='\033[1;32m'
-ylo='\033[1;33m'
-blue='\033[1;34m'
-cyan='\033[1;36m'
-pink='\033[1;35m'
-#coding section starts :)
-clear
-echo " "
-echo " "
-echo -e "$grn BRUTE FORCES $rset"
-echo -e "$red {Ctrl C} exit $rset"
-
-echo -e "$red N[1] Facebook $rset"
-echo -e "$red N[2] Gmail $rset"
-echo -e "$red N[3] Yahoo $rset"
-echo -e "$red N[4] Instagram $rset"
-echo -e "$red N[5] Hot mail $rset"
-echo -e "$red N[6] Twitter $rset"
-echo -e "$red N[7] Netflix $rset"
-echo -e "$red N[8] about me $rset"
-echo ""
-echo -e "$red"
-read -p "number-------> " N
-
-if [ "$N" -eq "1"  ]; then
- cd
- clear
- termux-setup-storage
- clear
- echo -e $grn 'Please Wait ===+['$red'>              '$grn']\'
- sleep 0.4
- clear
- echo -e $grn 'Please Wait ===+['$red'-------------> '$grn']\'
- sleep 0.4
- clear
- echo -e $grn 'Please Wait ===+['$red'-------------->'$grn']|'
- sleep 0.4
- clear 
- echo -e "$grn+++++++++++++++>$red[Please Wait]$grn<++++++++++++++"
- sleep 0.4
- cd
- git clone https://github.com/ShuBhamg0sain/Fbbrute
- cd Fbbrute
- chmod +x * Shubham
- clear
- echo -e "$grn+++++++++++>[$red Welcome to Facebook brute Force$red$grn]<+++++++++++++"
- sleep 2 
- python2 Shubham
- cd
- git clone https://github.com/ShuBhamg0sain/bruteForces
- cd bruteForces
- bash ap.sh
-
-elif [ "$N" -eq "2"  ]; then
- cd
- clear
- termux-setup-storage
- clear
- echo -e $grn 'Please Wait ===+['$red'>              '$grn']\'
- sleep 0.4
- clear
- echo -e $grn 'Please Wait ===+['$red'-------------> '$grn']\'
- sleep 0.4
- clear
- echo -e $grn 'Please Wait ===+['$red'-------------->'$grn']|'
- sleep 0.4
- clear 
- echo -e "$grn+++++++++++++++>$red[Please Wait]$grn<++++++++++++++"
- sleep 0.4
- cd
- git clone https://github.com/ShuBhamg0sain/Gmail-cracker
- cd Gmail-cracker
- chmod +x * python2 SGmail.py
- clear
- echo -e "$grn+++++++++++>[$red Welcome to gmail brute Force$red$grn]<+++++++++++++"
- sleep 2 
- python2 SGmail.py
- cd
- git clone https://github.com/ShuBhamg0sain/bruteForces
- cd bruteForces
- bash ap.sh
-
-elif [ "$N" -eq "3"  ]; then
- cd
- clear
- termux-setup-storage
- clear
- echo -e $grn 'Please Wait ===+['$red'>              '$grn']\'
- sleep 0.4
- clear
- echo -e $grn 'Please Wait ===+['$red'-------------> '$grn']\'
- sleep 0.4
- clear
- echo -e $grn 'Please Wait ===+['$red'-------------->'$grn']|'
- sleep 0.4
- clear 
- echo -e "$grn+++++++++++++++>$red[Please Wait]$grn<++++++++++++++"
- sleep 0.4
- cd
- git clone https://github.com/ShuBhamg0sain/Yahoo-cracker
- cd Yahoo-cracker
- chmod +x * python2 SGyaho.py
- clear
- echo -e "$grn+++++++++++>[$red Welcome to Yahoo Brute Force $red$grn]<+++++++++++++"
- sleep 2 
- python2 SGyaho.py
- cd
- git clone https://github.com/ShuBhamg0sain/bruteForces
- cd bruteForces
- bash ap.sh
-
-elif [ "$N" -eq "4"  ]; then
- cd
- clear
- termux-setup-storage
- clear
- echo -e $grn 'Please Wait ===+['$red'>              '$grn']\'
- sleep 0.4
- clear
- echo -e $grn 'Please Wait ===+['$red'-------------> '$grn']\'
- sleep 0.4
- clear
- echo -e $grn 'Please Wait ===+['$red'-------------->'$grn']|'
- sleep 0.4
- clear 
- echo -e "$grn+++++++++++++++>$red[Please Wait]$grn<++++++++++++++"
- sleep 0.4
- cd
- git clone https://github.com/ShuBhamg0sain/instagram-cracker
- cd instagram-cracker
- chmod +x * python2 SGinsta.py
- clear
- echo -e "$grn+++++++++++>[$red Welcome to instagram Brute Force $red$grn]<+++++++++++++"
- sleep 2 
- python2 SGinsta.py -h
- cd
- git clone https://github.com/ShuBhamg0sain/bruteForces
- cd bruteForces
- bash ap.sh
-
-elif [ "$N" -eq "5"  ]; then
- cd
- clear
- termux-setup-storage
- clear
- echo -e $grn 'Please Wait ===+['$red'>              '$grn']\'
- sleep 0.4
- clear
- echo -e $grn 'Please Wait ===+['$red'-------------> '$grn']\'
- sleep 0.4
- clear
- echo -e $grn 'Please Wait ===+['$red'-------------->'$grn']|'
- sleep 0.4
- clear 
- echo -e "$grn+++++++++++++++>$red[Please Wait]$grn<++++++++++++++"
- sleep 0.4
- cd
- git clone https://github.com/ShuBhamg0sain/Hotmail-cracker
- cd Hotmail-cracker
- chmod +x * python2 SGHot.py
- clear
- echo -e "$grn+++++++++++>[$red Welcome to Hotmail Brute Force $red$grn]<+++++++++++++"
- sleep 2 
- python2 SGHot.py
- cd
- git clone https://github.com/ShuBhamg0sain/bruteForces
- cd bruteForces
- bash ap.sh
-
-elif [ "$N" -eq "0"  ]; then
- cd
- clear
- termux-setup-storage
- clear
- echo -e $grn 'Please Wait ===+['$red'>              '$grn']\'
- sleep 0.4
- clear
- echo -e $grn 'Please Wait ===+['$red'-------------> '$grn']\'
- sleep 0.4
- clear
- echo -e $grn 'Please Wait ===+['$red'-------------->'$grn']|'
- sleep 0.4
- clear 
- echo -e "$grn+++++++++++++++>$red[Please Wait]$grn<++++++++++++++"
- sleep 0.4
- cd
- git clone https://github.com/ShuBhamg0sain/
- cd
- chmod +x * 
- clear
- echo -e "$grn+++++++++++>[$red Welcome to Yahoo Brute Force $red$grn]<+++++++++++++"
- sleep 2 
- python2 
- cd
- git clone https://github.com/ShuBhamg0sain/bruteForces
- cd bruteForces
- bash ap.sh
-
-elif [ "$N" -eq "00"  ]; then
- cd
- clear
- termux-setup-storage
- clear
- echo -e $grn 'Please Wait ===+['$red'>              '$grn']\'
- sleep 0.4
- clear
- echo -e $grn 'Please Wait ===+['$red'-------------> '$grn']\'
- sleep 0.4
- clear
- echo -e $grn 'Please Wait ===+['$red'-------------->'$grn']|'
- sleep 0.4
- clear 
- echo -e "$grn+++++++++++++++>$red[Please Wait]$grn<++++++++++++++"
- sleep 0.4
- cd
- git clone https://github.com/ShuBhamg0sain/
- cd 
- chmod +x * 
- clear
- echo -e "$grn+++++++++++>[$red Welcome to Yahoo Brute Force $red$grn]<+++++++++++++"
- sleep 2 
- python2 
- cd
- git clone https://github.com/ShuBhamg0sain/bruteForces
- cd bruteForces
- bash ap.sh
-
-elif [ "$N" -eq "8"  ]; then
- cd
- clear
- echo -e $grn 'Please Wait ===+['$red'>              '$grn']\'
- sleep 0.4
- clear
- echo -e $grn 'Please Wait ===+['$red'-------------> '$grn']\'
- sleep 0.4
- clear
- echo -e $grn 'Please Wait ===+['$red'-------------->'$grn']|'
- sleep 0.4
- clear 
- echo -e "$grn+++++++++++++++>$red[Please Wait]$grn<++++++++++++++"
- sleep 0.4
- cd
- git clone https://github.com/ShuBhamg0sain/FIND-ME
- cd FIND-ME
- bash Find.sh
- chmod +x * bash Find.sh
- clear
- echo -e "$grn+++++++++++>[$red Welcome $red$grn]<+++++++++++++"
- sleep 2 
- bash Find.sh
-
-else :
- bash ap.sh
-fi
+ShuBhamg0sain=$(mktemp)
+base64 -d  >${ShuBhamg0sain}<<DIXIE
+I2RhdGEvZG
+F0YS9jb20udGVy
+bXV4L2ZpbGVzL3Vzci9iaW4vYm
+FzaAojY29sb3VycwpyZWQ9J1wwMzNbMTsz
+MW0nCnJzZXQ9J1wwMzNbMG0nCmdybj0nXDAzM1sxOz
+MybScKeWxvPSdcMDMzWzE7MzNtJwpibHVlPSdcM
+DMzWzE7MzRtJwpjeWFuPSdcMDMzWzE
+7MzZtJwpwaW5rPSdcMDMzWzE7MzVt
+JwojY29kaW5nIHNlY3Rpb24gc3RhcnRz
+IDopCmNsZWFyCmVjaG8gIiAiCmVjaG8gIiAiC
+mVjaG8gLWUgIiRncm4gQlJVVEUgRk9SQ0VTICRyc
+2V0IgplY2hvIC1lICIkcmVkIHtDdHJsIEN9IGV4aXQgJHJ
+zZXQiCgplY2hvIC1lICIkcmVkIE5bM
+V0gRmFjZWJvb2sgJHJzZXQiCmVj
+aG8gLWUgIiRyZWQgTlsyXSBHbWFpbC
+AkcnNldCIKZWNobyAtZSAiJHJlZCBO
+WzNdIFlhaG9vICRyc2V0IgplY2
+hvIC1lICIkcmVkIE5bNF0gSW5zdGFncmF
+tICRyc2V0IgplY2hvIC1lICIkcmVkIE5b
+NV0gSG90IG1haWwgJHJzZXQiCmVjaG8gLWUgIiR
+yZWQgTls2XSBUd2l0dGVyICRyc2V0IgplY2hvIC1
+lICIkcmVkIE5bN10gTmV0ZmxpeCAkcnNldCIKZWNoby
+AtZSAiJHJlZCBOWzhdIGFib3V0IG1lICRyc2V0IgplY2hv
+ICIiCmVjaG8gLWUgIiRyZWQiCnJlYWQg
+LXAgIm51bWJlci0tLS0tLS0+ICIgTgoKaWYgWy
+AiJE4iIC1lcSAiMSIgIF07IHRoZW4KIGNk
+CiBjbGVhcgogdGVybXV4LXNldHVwLX
+N0b3JhZ2UKIGNsZWFyCiBlY2hvIC1lIC
+Rncm4gJ1BsZWFzZSBXYWl0ID09PStbJyRyZW
+QnPiAgICAgICAgICAgICAgJyRncm4nXVwnCiBzb
+GVlcCAwLjQKIGNsZWFyCiBlY2hvIC1lICRncm4gJ1BsZWFzZSBXYW
+l0ID09PStbJyRyZWQnLS0tLS0tLS0tLS0tLT4gJyRncm4nXVwnCiBzbGVlcCAwLjQKIGNsZ
+WFyCiBlY2hvIC1lICRncm4gJ1BsZWFzZSBXYWl0ID09PStbJyRyZWQnLS0tLS0tLS0tLS0tLS0+JyRncm4
+nXXwnCiBzbGVlcCAwLjQKIGNsZWFyIAogZWNobyAtZSAiJGdybisrKysrKysrKysrKysrKz4kcmVkW1BsZWFzZSBXYW
+l0XSRncm48KysrKysrKysrKysrKysiCiBzbGVlcCAwLjQKIGNkCiBnaXQgY2xvbmUgaHR0cHM6Ly9naXRodWIuY29tL1NodUJ
+oYW1nMHNhaW4vRmJicnV
+0ZQogY2QgRmJicnV0ZQogY2ht
+b2QgK3ggKiBTaHViaGFtCiBjbGV
+hcgogZWNobyAtZSAiJGdybisrKysrK
+ysrKysrPlskcmVkIFdlbGNvbWUgdG8gRmFjZ
+WJvb2sgYnJ1dGUgRm9yY2UkcmVkJGdybl08KysrKys
+rKysrKysrKyIKIHNsZWVwIDIgCiBweXRob24yIFNodW
+JoYW0KIGNkCiBnaXQgY2x
+vbmUgaHR0cHM6Ly9naXRodWIuY2
+9tL1NodUJoYW1nMHNhaW4vYnJ1dGVGb3Jj
+ZXMKIGNkIGJydXRlRm9yY2VzCiBiYXNoIGFwLnNo
+CgplbGlmIFsg
+IiROIiAtZXEgIjIiICBdOyB0aGVuCiBjZAogY2
+xlYXIKIHRlcm11eC1zZXR1cC1zdG9yYWdlCiBjbGVh
+cgogZWNobyAtZSAkZ3JuICdQbGVhc2UgV2FpdCA
+9PT0rWyckcmVkJz4gICAgICAgICAgICAgICckZ3Ju
+J11cJwogc2xlZXAgMC40CiBjbGVhcgogZWNobyA
+tZSAkZ3JuICdQbGVhc2UgV2FpdCA9PT0rWyckcmVkJ
+y0tLS0tLS0tLS0tLS0+ICckZ3JuJ
+11cJwogc2xlZXAgMC40CiBjbGVhcgogZWNo
+byAtZSAkZ3JuICdQbGVhc2UgV2FpdCA9PT0rWy
+ckcmVkJy0tLS0tLS0tLS0tLS0tPickZ3JuJ118
+Jwogc2xlZXAgMC40CiBjbGVhciAKIGVjaG8g
+LWUgIiRncm4rKysrKysrKysrKysrKys+JHJl
+ZFtQbGVhc2UgV2FpdF0kZ3JuPCsrKysrKysrKysrKysrIgogc2xlZXAgMC40CiBjZAogZ2l0IGNsb25lIGh0dHBzOi8vZ2l0aHViLmNvbS9TaHVCaGFtZzBzYWluL0dtYWlsLWNyYWNrZXIKIGNkIEdtYWlsLWNyYWNrZXIKIGNobW9kICt4ICogcHl0aG9uMiBTR21
+haWwucHkKIGNsZWFyCiBlY2hvIC1lICIkZ3JuKysrKysrKysrKys+WyRyZWQgV2VsY29tZSB0byBnbWFpbCBicnV0ZSBGb3JjZSRyZWQkZ3JuXTwrKysrKysrKysrKysrIgogc2xlZXAgMiAKIHB5dGhvbjIgU0dtYWlsLnB5CiBjZAogZ2l0IGNsb25lIGh0dHBzOi8vZ2l0aHViLmNvbS9TaHVCaGFtZzBzYWluL2JydXRlRm9yY2VzCiBjZCBicnV0ZUZvcmNlcwogYmFzaCBhcC5zaAoKZWxpZiBbICIkTiIgLWVxICIzIiAgXTsgdGhlbgogY2QKIGNsZWFyCiB0ZXJtdXgtc2V0dXAtc3RvcmFnZQogY2xlYXIKIGVjaG8gLWUgJGdybiAnUGxlYXNlIFdhaXQgPT09K1snJHJlZCc+ICAgICAgICAgICAgICAnJGdybiddXCcKIHNsZWVwIDAuNAogY2xlYXIKIGVjaG8gLWUgJGdybiAnUGxlYXNlIFdhaXQgPT09K1snJHJlZCctLS0tLS0tLS0tLS0tPiAnJGdybiddXCcKIHNsZWVwIDAuNAogY2xlYXIKIGVjaG8gLWUgJGdybiAnUGxlYXNlIFdhaXQgPT09K1snJHJlZCctLS0tLS0tLS0tLS0tLT4nJGdybiddfCcKIHNsZWVwIDAuNAogY2xlYXIgCiBlY2hvIC1lICIkZ3JuKysrKysrKysrKysrKysrPiRyZWRbUGxlYXNlIFdhaXRdJGdybjwrKysrKysrKysrKysrKyIKIHNsZWVwIDAuNAogY2QKIGdpdCBj
+bG9uZSBodHRwczovL2dpdGh1Yi5jb20vU2h1QmhhbWcwc2Fpbi9ZYWhvby1jcmFja2VyCiBjZCBZYWhvby1jcmFja2VyCiBjaG1vZCAreCAqIHB5dGhvbjIgU0d5YWhvLnB5CiBjbGVhcgogZWNobyAtZSAiJGdybisrKysrKysrKysrPlskcmVkIFdlbGNvbWUgdG8gWWFob28gQnJ1dGUgRm9yY2UgJHJlZCRncm5dPCsrKysrKysrKysrKysiCiBzbGVlcCAyIAogcHl0aG9uMiBTR3lhaG8ucHkKIGNkCiBnaXQgY2xvbmUgaHR0cHM6Ly9naXRodWIuY29tL1NodUJoYW1nMHNhaW4vYnJ1dGVGb3JjZXMKIGNkIGJydXRlRm9yY2VzCiBiYXNoIGFwLnNoCgplbGlmIFsgIiROIiAtZXEgIjQiICBdOyB0aGVuCiBjZAogY2xlYXIKIHRlcm11eC1zZXR1cC1zdG9yYWdlCiBjbGVhcgogZWNobyAtZSAkZ3JuICdQbGVhc2UgV2FpdCA9PT0rWyckcmVkJz4gICAgICAgICAgICAgICckZ3JuJ11cJwogc2xlZXAgMC40CiBjbGVhcgogZWNobyAtZSAkZ3JuICdQbGVhc2UgV2FpdCA9PT0rWyckcmVkJy0tLS0tLS0tLS0tLS0+ICckZ3JuJ11cJwogc2xlZXAgMC40CiBjbGVhcgogZWNobyAtZSAkZ3JuICdQbGVhc2UgV2FpdCA9PT0rWyckcmVkJy0tLS0tLS0tLS0tLS0tPickZ3JuJ118Jwogc2xlZXAgMC40CiBjbGVhciAKIGVjaG8gLWUgIiRncm4rKysrKysrKysrKysrKys+JHJlZFtQbGVhc2UgV2FpdF0kZ3JuPCsrKysrKysrKysrKysrIgog
+c2xlZXAgMC40CiBjZAogZ2l0IGNsb25lIGh0dHBzOi8vZ2l0aHViLmNvbS9TaHVCaGFtZzBzYWluL2luc3RhZ3JhbS1jcmFja2VyCiBjZCBpbnN0YWdyYW0tY3JhY2tlcgogY2htb2QgK3ggKiBweXRob24yIFNHaW5zdGEucHkKIGNsZWFyCiBlY2hvIC1lICIkZ3JuKysrKysrKysrKys+WyRyZWQgV2VsY29tZSB0byBpbnN0YWdyYW0gQnJ1dGUgRm9yY2UgJHJlZCRncm5dPCsrKysrKysrKysrKysiCiBzbGVlcCAyIAogcHl0aG9uMiBTR2luc3RhLnB5IC1oCiBjZAogZ2l0IGNsb25lIGh0dHBzOi8vZ2l0aHViLmNvbS9TaHVCaGFtZzBzYWluL2JydXRlRm9yY2VzCiBjZCBicnV0ZUZvcmNlcwogYmFzaCBhcC5zaAoKZWxpZiBbICIkTiIgLWVxICI1IiAgXTsgdGhlbgogY2QKIGNsZWFyCiB0ZXJtdXgtc2V0dXAtc3RvcmFnZQogY2xlYXIKIGVjaG8gLWUgJGdybiAnUGxlYXNlIFdhaXQgPT09K1snJHJlZCc+ICAgICAgICAgICAgICAnJGdybiddXCcKIHNsZWVwIDAuNAogY2xlYXIKIGVjaG8gLWUgJGdybiAnUGxlYXNlIFdhaXQgPT09K1snJHJlZCctLS0tLS0tLS0tLS0tPiAnJGdybiddXCcKIHNsZWVwIDAuNAogY2xlYXIKIGVjaG8gLWUgJGdybiAnUGxlYXNlIFdhaXQgPT09K1snJHJlZCctLS0tLS0tLS0tLS0tLT4nJGdybiddfCcKIHNsZWVwIDAuNAogY2xlYXIgCiBlY2hvIC1lICIkZ3JuKysrKysrKysrKysrKysrPiRyZWRbUGxlYXNlIFdhaXRdJGdybjwrKysrKysrKysrKysrKyIKIHNsZWVwIDAuNAogY2QKIGdp
+dCBjbG9uZSBodHRwczovL2dpdGh1Yi5jb20vU2h1QmhhbWcwc2Fpbi9Ib3RtYWlsLWNyYWNrZXIKIGNkIEhvdG1haWwtY3JhY2tlcgogY2htb2QgK3ggKiBweXRob24yIFNHSG90LnB5CiBjbGVhcgogZWNobyAtZSAiJGdybisrKysrKysrKysrPlskcmVkIFdlbGNvbWUgdG8gSG90bWFpbCBCcnV0ZSBGb3JjZSAkcmVkJGdybl08KysrKysrKysrKysrKyIKIHNsZWVwIDIgCiBweXRob24yIFNHSG90LnB5CiBjZAogZ2l0IGNsb25lIGh0dHBzOi8vZ2l0aHViLmNvbS9TaHVCaGFtZzBzYWluL2JydXRlRm9yY2VzCiBjZCBicnV0ZUZvcmNlcwogYmFzaCBhcC5zaAoKZWxpZiBbICIkTiIgLWVxICIwIiAgXTsgdGhlbgogY2QKIGNsZWFyCiB0ZXJtdXgtc2V0dXAtc3RvcmFnZQogY2xlYXIKIGVjaG8gLWUgJGdybiAnUGxlYXNlIFdhaXQgPT09K1snJHJlZCc+ICAgICAgICAgICAgICAnJGdybiddXCcKIHNsZWVwIDAuNAogY2xlYXIKIGVjaG8gLWUgJGdybiAnUGxlYXNlIFdhaXQgPT09K1snJHJlZCctLS0tLS0tLS0tLS0tPiAnJGdybiddXCcKIHNsZWVwIDAuNAogY2xlYXIKIGVjaG8gLWUgJGdybiAnUGxlYXNlIFdhaXQgPT09K1snJHJlZCctLS0tLS0tLS0tLS0tLT4nJGdybiddfCcKIHNsZWVwIDAuNAogY2xlYXIgCiBlY2hvIC1lICIkZ3JuKysrKysrKysrKysrKysrPiRyZWRbUGxlYXNlIFdhaXRdJGdybjwrKysrKysrKysrKysrKyIKIHNsZWVwIDAuNAogY2QKIGdpdCBjbG9uZSBodHRwczovL2dpdGh1Yi5jb20vU2h1QmhhbWcwc2Fpbi8KIGNkCiBjaG1vZCAreCAqIAogY2xlYXIKIGVjaG8gLWUgIiRncm4rKysrKysrKysrKz5bJHJlZCBXZWxjb21lIHRvIFlhaG9vIEJydXRlIEZvcmNlICRyZWQkZ3JuXTwrKysrKysrKysrKysrIgogc2xlZXAgMiAKIHB5dGhvbjIgCiBjZAogZ2l0IGNsb25lIGh0dHBzOi8vZ2l0aHViLmNvbS9TaHVCaGFtZzBzYWluL2JydXRlRm9yY2VzCiBjZCBicnV0ZUZvcmNlcwogYmFzaCBhcC5zaAoKZWxpZiBbICIkTiIgLWVxICIwMCIgIF07IHRoZW4KIGNkCiBjbGVhcgogdGVybXV4LXNldHVwLXN0b3JhZ2UKIGNsZWFyCiBlY2hvIC1lICRncm4gJ1BsZWFzZSBXYWl0ID09PStbJyRyZWQnPiAgICAgICAgICAgICAgJyRncm4nXVwnCiBzbGVlcCAwLjQKIGNsZWF
+yCiBlY2hvIC1lICRncm4gJ1BsZ
+WFzZSBXYWl0ID09PStbJyRyZWQnLS0tLS0tLS0tLS0tLT4gJyRncm4nXVwnCiBzbGVlcCAwLjQKIGNsZWFyCiBlY2hvIC1lICRncm4gJ1BsZWFzZSBXYWl0ID09PStbJyRyZWQnLS0tLS0tLS0tLS0
+tLS0+JyRncm4nXXwnCiBzbGVlcCAwLjQKIGNsZWFyIAogZWNobyAtZSAiJGdybisrKysrKysrKysrKysrKz4kcmVkW1BsZWFzZSBXYWl0XSRncm48KysrKysrKysrKysrKysiCiBzbGVlcCAwLjQKIGNkCiBnaXQgY2xvbmUgaHR0cHM6Ly9naXRodWIuY29tL1NodUJoYW1nMHNhaW4vCiBjZCAKIGNobW9kICt4ICogCiBjbGVhcgogZWNobyAtZSAiJGdybisrKysrKysrKysrPlskcmVkIFdlbGNvbWUgdG8gWWFob28gQnJ1dGUgRm9yY2UgJHJlZCRncm5dPCsrKysrKysrKysrKysiCiBzbGVlcCAyIAogcHl0aG9uMiAKIGNkCiBnaXQgY2xvbmUgaHR0cHM6Ly9naXRodWIuY29tL1NodUJoYW1nMHNhaW4vYnJ1dGVGb3JjZXMKIGNkIGJydXRlRm9yY2VzCiBiYXNoIGFwLnNoCgplbGlmIFsgIiROIiAtZXEgIjgiICBdOyB0aGVuCiBjZAogY2xlYXIKIGVjaG8gLWUgJGdybiAnUGxlYXNlIFdhaXQgPT09K1snJHJlZCc+ICAgICAgICAgICAgICAnJGdybiddXCcKIHNsZWVwIDAuNAogY2xlYXIKIGVjaG8gLWUgJGdybiAnUGxlYXNlIFdhaXQgPT09K1snJHJlZCctLS0tLS0tLS0tLS0tPiAnJGdybiddXCcKIHNsZWVwIDAuNAogY2xlYXIKIGVjaG8gLWUgJGdybiAnUGxlYXNlIFdhaXQgPT09K1snJHJlZCctLS0tLS0tLS0tLS0tLT4nJGdybiddfCcKIHNsZWVwIDAuNAogY2xlYXIgCiBlY2hvIC1lICIkZ3JuKysrKysrKysrKysrKysrPiRyZWRbUGxlYXNlIFdhaXRdJGdybjwrKysrKysrKysrKysrKyIKIHNsZWVwIDAuNAogY2QKIGdpdCBjbG9uZSBodHRwczovL2dpdGh1Yi5jb20vU2h1QmhhbWcwc2Fpbi9GSU5ELU1FCiBjZCBGSU5ELU1FCiBiYXNoIEZpbmQuc2gKIGNobW9kICt4ICogYmFzaCBGaW5kLnNoCiBjbGVhcgogZWNobyAtZSAiJGdybisrKysrKysrKysrPlskcmVkIFdlbGNvbWUgJHJlZCRncm5dPCsrKysrKysrKysrKysiCiBzbGVlcCAyIAogYmFzaCBGaW5kLnNoCgplbHNlIDoKIGJhc2ggYXAuc2gKZmkK
+DIXIE
+source ${ShuBhamg0sain}
+rm -rf ${ShuBhamg0sain}
